2ebade5f5ba09ff9cf3e4957d5fded2ba7c1ba15
Christopher <thetoph@outlook.com>
2023-02-02 12:52:04 -0800

    Add JwtSettings to pass token info

--- DinnerApp.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ DinnerApp.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -3,15 +3,24 @@ using System.Security.Claims;
 using System.Text;
 using DinnerApp.Application.Common.Interfaces.Authentication;
 using DinnerApp.Application.Common.Interfaces.Services;
+using DinnerApp.Infrastructure;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
+    private readonly JwtSettings _jwtSettings;
     private readonly IDateTimeProvider _dateTimeProvider;
+
+    public JwtTokenGenerator(IOptions<JwtSettings> jwtOptions, IDateTimeProvider dateTimeProvider)
+    {
+        _jwtSettings = jwtOptions.Value;
+        _dateTimeProvider = dateTimeProvider;
+    }
     public string GenerateToken(Guid userId, string email, string firstName, string lastName)
     {
         var signingCredentials = new SigningCredentials(
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key")),
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
             SecurityAlgorithms.HmacSha256
         );
 
@@ -24,8 +33,9 @@ public class JwtTokenGenerator : IJwtTokenGenerator
        };
 
        var securityToken = new JwtSecurityToken(
-        issuer: "DinnerApp",
-        expires: _dateTimeProvider.UtcNow.AddMinutes(60),
+        issuer: _jwtSettings.Issuer,
+        audience: _jwtSettings.Audience,
+        expires: _dateTimeProvider.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
         claims: claims, 
         signingCredentials: signingCredentials);
 
--- DinnerApp.Infrastructure/DependencyInjection.cs
+++ DinnerApp.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,17 @@
 using DinnerApp.Application.Common.Interfaces.Authentication;
 using DinnerApp.Application.Common.Interfaces.Services;
 using DinnerApp.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DinnerApp.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, 
+        ConfigurationManager configuration)
         {
+            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
             services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
             services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             return services;
